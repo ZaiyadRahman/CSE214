35adf11bc13e27a3a2e3eb863a3244d25b6467ef
Neoscoper <98933712+Neoscoper@users.noreply.github.com>
2022-08-26 19:56:55 -0400

    Start to the CSE 214 course

---
+++ src/com/company/Lab1/Course.java
@@ -0,0 +1,5 @@
+package com.company.Lab1;public class Course {
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ src/com/company/Lab1/Planner.java
@@ -0,0 +1,5 @@
+package com.company.Lab1;public class Planner {
+    public static void main(String[] args) {
+    
+    }
+}
